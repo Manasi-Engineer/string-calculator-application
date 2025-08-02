3cd7afd124c93d77752e9fb2ddf6a80ff70230cb
Manasi-Engineer <manasi.engineer@gmail.com>
2025-08-02 12:27:01 +0530

    Added module change to handle the case

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,11 @@
 
 class StringCalculator
   def add(numbers)
+    # String with no delimiter only number
     return numbers.to_i if is_numeric_regex?(numbers)
+
+    # String with deplimiter as ','
+    numbers.split(',').map(&:to_i).sum
   end
 
   def is_numeric_regex?(str)
